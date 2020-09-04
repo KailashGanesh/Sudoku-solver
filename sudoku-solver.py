c58e3c1b04ae5894f76c2d45ba64bfb527aead36
Kailash <58307685+KailashGanesh@users.noreply.github.com>
2020-09-05 00:46:25 +0530

    EveryThing works

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -9,10 +9,8 @@ board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
-
-b2 =[[1,2,3],[1,2,3],[1,2,3]]
 # function to print the board in cli
-def printBoard(board):
+def print_board(board):
     for i in range(len(board)):
         if i%3 == 0 and i != 0:
             print("- - - - - - - - - - - - - - - - ")
@@ -35,31 +33,53 @@ def find_empty_spot(board):
     return False
 
 # is this a valid move
-def is_valid_move(board, pos, num):
-    row, col = pos
-
+def is_valid_move(board, num, pos):
+    row,col = pos
     # checking row
-    if num in board[row]:
-        return False
+    for i in range(len(board[0])):
+        if board[row][i] == num and col != i:
+            return False
 
     # checking column
     for i in range(len(board)):
-        if num == board[i][col]:
+        if board[i][col] == num and row != i:
             return False
     
     # checking the box
-    box_x = row // 3
-    box_y = col // 3
+    box_x = col // 3
+    box_y = row // 3
 
     for i in range(box_y*3,box_y*3 + 3):
         for j in range(box_x*3,box_x*3 + 3):
-            if board[i][j] == num:
+            if board[i][j] == num and (i,j) != pos:
                 return False
     return True
 
 
 
-
 # slove with backtracking algo
-printBoard(board)
-print(find_empty_spot(board))
+def solve_board(board):
+    spot = find_empty_spot(board)
+
+    if spot == False:
+        return True
+    else:
+        row, col = spot
+
+    for i in range(1,10):
+        if is_valid_move(board,i,(row,col)):
+            board[row][col] = i
+
+            if solve_board(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+print_board(board)
+# slove_board(board)
+solve_board(board)
+print("sloved")
+print_board(board)
